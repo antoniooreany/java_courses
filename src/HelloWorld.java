ec396f17515e2e14a9c25d2a7789a8451b44ab73
Anton Gorshkov <antonio_oreany@mail.ru>
2016-10-04 15:45:47 +0300

    CSS Inheritance

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,16 @@
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
 /**
  * Created by gorshkov on 08.09.2016.
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String abc = new String("abc");
+        String abc = "abc";
         String xyz = "xyz";
+        Calendar now = new GregorianCalendar(TimeZone.getTimeZone(ZoneId.systemDefault()));
     }
 }
