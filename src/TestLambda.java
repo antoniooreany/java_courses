75a4801b6a1b0fac5d5264d4b2bcb93bce5e86e2
Anton Gorshkov <antonio_oreany@mail.ru>
2017-02-03 12:32:00 +0300

    TestLambda with anonimous class new Operation(){}

--- src/TestLambda.java
+++ src/TestLambda.java
@@ -16,7 +16,12 @@ public class TestLambda {
 
         Map<String, Operation> operations = new HashMap<>();
 
-        operations.put(plusSign, (a, b) -> a + b);
+        operations.put(plusSign, new Operation() {
+            @Override
+            public int operate(int a, int b) {
+                return a + b;
+            }
+        });
         operations.put(minusSign, (a, b) -> a - b);
         operations.put(powSign, (a, b) -> (int) Math.pow(a, b));
 
